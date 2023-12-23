c0ff3e87ff0d3f048ca3a6e07025fd854195902f
Jacob Stone <jumboshrimp9554@gmail.com>
2023-12-22 19:46:32 -0800

    pushing first iteration of the project. It is functional!

---
+++ ML/MLHandler.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.ML;
+using Microsoft.ML.Data;
+using static Microsoft.ML.DataOperationsCatalog;
+
+
+
+
+
+namespace SentimentAnalyzerWebApp.ML
+{
+    public class MLHandler
+    {
+        string _dataPath = Path.Combine(Environment.CurrentDirectory, "Data", "yelp_labelled.txt");
+
+        MLContext mlContext;
+        TrainTestData splitDataView;
+        ITransformer model;
+
+        public MLHandler()
+        {
+            mlContext = new MLContext();
+            splitDataView = LoadData(mlContext);
+            model = BuildAndTrainModel(mlContext, splitDataView.TrainSet);
+        }
+
+        TrainTestData LoadData(MLContext mlContext)
+        {
+            IDataView dataView = mlContext.Data.LoadFromTextFile<SentimentData>(_dataPath, hasHeader: false);
+            TrainTestData splitDataView = mlContext.Data.TrainTestSplit(dataView, testFraction: 0.2);
+
+            return splitDataView;
+        }
+
+        ITransformer BuildAndTrainModel(MLContext mlContext, IDataView splitTrainSet)
+        {
+            var estimator = mlContext.Transforms.Text.FeaturizeText(outputColumnName: "Features", inputColumnName: nameof(SentimentData.SentimentText))
+                .Append(mlContext.BinaryClassification.Trainers.SdcaLogisticRegression(labelColumnName: "Label", featureColumnName: "Features"));
+            Console.WriteLine("=============== Create and Train the Model ===============");
+            var model = estimator.Fit(splitTrainSet);
+            Console.WriteLine("=============== End of training ===============");
+            Console.WriteLine();
+            return model;
+        }
+
+        //you need to pass in sentimentData from the index page when this method is called.
+        //removed MLContext mlContext, ITransformer model from method parameters
+        public string UseModelWithSingleItem(SentimentData sentimentData)
+        {
+            PredictionEngine<SentimentData, SentimentPrediction> predictionFunction = mlContext.Model.CreatePredictionEngine<SentimentData, SentimentPrediction>(model);
+
+            var resultPrediction = predictionFunction.Predict(sentimentData);
+
+            Console.WriteLine();
+            Console.WriteLine("=============== Prediction Test of model with a single sample and test dataset ===============");
+
+            Console.WriteLine();
+            Console.WriteLine($"Sentiment: {resultPrediction.SentimentText} | Prediction: {(Convert.ToBoolean(resultPrediction.Prediction) ? "Positive" : "Negative")} | Probability: {resultPrediction.Probability} ");
+            var result = $"Sentiment: {resultPrediction.SentimentText} | Prediction: {(Convert.ToBoolean(resultPrediction.Prediction) ? "Positive" : "Negative")} | Probability: {resultPrediction.Probability} ";
+            Console.WriteLine("=============== End of Predictions ===============");
+            Console.WriteLine();
+
+            return result;
+        }
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SentimentAnalyzerWebApp.ML;
+
+namespace SentimentAnalyzerWebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty]
+        public string? input { get; set; }
+
+        public SentimentData sentimentData { get; set; }
+        public SentimentPrediction prediction { get; set; }
+        public MLHandler mlHandler { get; set; }
+        public string result;
+        public string test { get; set; }
+
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+            sentimentData = new SentimentData();
+            mlHandler = new MLHandler();
+            prediction = new SentimentPrediction();
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public IActionResult OnPost()
+        {
+            sentimentData.SentimentText = input;
+
+            
+
+            result = mlHandler.UseModelWithSingleItem(sentimentData);
+
+
+            return Page();
+        }
+    }
+}
+
+
+/*
+ * 
+ * you need to:
+ * ->bind an input string to string variable.
+ * ->create an instance of SentimentData.
+ * ->assign that string to SentimentData.SentimentText
+ * ->create an instance of ML Handler
+ * ->call all of the necessary functions to train the ML model (did so in the constructor)
+ * ->call UseModelWithSingleItem, passing in the SentimentData previously created
+ * ->assign the result of UseModelWithSingleItem to a SentimentPrediction
+ * ->Print that sentiment prediction on the cshtml view
+ */
